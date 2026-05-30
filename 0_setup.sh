63c49f3cdc4cb2eeca50daaf05c24d15aa9f7e16
Dianah Anderson <dma88713@uga.edu>
2026-05-29 22:55:52 -0400

    Script for extracting full fastq/raw reads

---
+++ 0_setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#Data should hold all orginal and raw/major intermediate files
+#Results holds all the analytical outcomes & figures
+ mkdir data results
+
+# Retrieve raw data files
+
+mkdir data/untrimmed_fastq
+cd data/untrimmed_fastq
+
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_2.fastq.gz
+cd ../../
